2254c89091177dfe5ca8f714f87a4f2c84a25e5d
EscapeRPG <syrphin@syrphin.com>
2025-05-16 09:04:17 +0200

    removed onclick events

--- aventures/lastparty/includes/nav.php
+++ aventures/lastparty/includes/nav.php
@@ -1,6 +1,8 @@
 <nav>
     <img src="/escaperpg/images/lastparty/jonathan.png" alt="jonathan">
-    <div id="inventairefooter"><input type="submit" onclick="inventaireshow()" value="INVENTAIRE"></div>
-    <div id="motsdepasse"><input type="submit" onclick="mdpshow()" value="NOTES"></div>
-    <a href="/escaperpg/aventures/lastparty/save/save.php" target="_blank" rel="noreferrer"><input type="submit" name="save" value="SAUVEGARDER"></a>
+    <div id="inventairefooter"><input type="submit" value="INVENTAIRE"></div>
+    <div id="motsdepasse"><input type="submit" value="NOTES"></div>
+    <a href="/escaperpg/aventures/lastparty/save/save.php" target="_blank" rel="noreferrer">
+        <input type="submit" name="save" value="SAUVEGARDER">
+    </a>
 </nav>
