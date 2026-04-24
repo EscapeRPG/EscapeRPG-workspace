adbc0512ec0832351a0948366999bf566481ad30
EscapeRPG <escaperpg@escaperpg.com>
2026-04-24 12:22:58 +0200

    save

---
+++ views/home/index.php
@@ -0,0 +1,13 @@
+<?php use App\Core\View; ?>
+
+<?php View::start('styles'); ?>
+<?php View::end(); ?>
+
+<?php View::start('content'); ?>
+<p>
+    Test page d'index
+</p>
+<?php View::end(); ?>
+
+<?php View::start('scripts'); ?>
+<?php View::end(); ?>
